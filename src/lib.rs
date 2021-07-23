cecf8abd1d124900ed230604152adf0f8866d111
Alex <alex.yaorh@gmail.com>
2021-07-23 14:48:45 -0500

    downgrade

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,8 @@
 //!
 //! See the [Users Guide](https://rust-lang-nursery.github.io/rust-bindgen/) for
 //! additional documentation.
-#![deny(missing_docs)]
-#![deny(warnings)]
+// #![deny(missing_docs)]
+// #![deny(warnings)]
 #![deny(unused_extern_crates)]
 // To avoid rather annoying warnings when matching with CXCursor_xxx as a
 // constant.
